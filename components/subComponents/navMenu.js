5df0c21192c37cd742e0e29d6bbb7eb925753688
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2022-12-10 15:48:47 +0100

    UnLogged nav menu structuring

--- components/subComponents/navMenu.js
+++ components/subComponents/navMenu.js
@@ -4,6 +4,60 @@ import React, { useState, useEffect, useCallback, useContext } from "react";
 import Link from "next/link";
 import { UserContext } from "../../context/userContext";
 
+const menuLogin = [
+  {
+    path: "/login",
+    title: "Identifiez-vous",
+  },
+  {
+    path: "/compatibility",
+    title: "Appareils Compatibles",
+  },
+  {
+    path: "/help",
+    title: "Aide",
+  },
+];
+
+const menuLanguages = [
+  {
+    path: "/french",
+    title: "Français",
+  },
+  {
+    path: "/english",
+    title: "English",
+  },
+  {
+    path: "/deutch",
+    title: "Deutsch",
+  },
+  {
+    path: "/norge",
+    title: "Norge",
+  },
+  {
+    path: "/suomi",
+    title: "Suomi",
+  },
+
+  {
+    path: "/portugues",
+    title: "Portugues",
+  },
+  {
+    path: "/espanol",
+    title: "Espagnol",
+  },
+  {
+    path: "/arabic",
+    title: "Arabic",
+  },
+  {
+    path: "/zoulou",
+    title: "Zoulou",
+  },
+];
 const menuParameters = [
   {
     path: "/settings",
@@ -49,7 +103,7 @@ const menuProfils = [
 export const NavMenu = ({}) => {
   const { currentUser } = useContext(UserContext);
 
-  return !currentUser ? (
+  return currentUser ? (
     <div className="inline-block max-w-navMW overflow-visible relative pr-4 cursor-pointer">
       <input type="checkbox" id="nav_profiles_dropdown" />
       <label
@@ -103,6 +157,80 @@ export const NavMenu = ({}) => {
       </ul>
     </div>
   ) : (
-    <div>Hello Disconnected</div>
+    <div className="flex">
+      <div className=" px-6">
+        <input type="checkbox" id="nav_profiles_dropdown_lang" />
+        <label
+          htmlFor="nav_profiles_dropdown_lang"
+          className="nav_profiles_dropdown_lang flex items-center relative pb-0 pl-0 font-normal m-0 border-none cursor-pointer"
+        >
+          <div className="flex items-center shrink-0 cursor-pointer opacity-70">
+            <img src="./welcome/globe-2-24.png" alt="user-icon" />
+          </div>
+          <div className="mx-2 overflow-hidden text-left text-ellipsis font-normal cursor-pointer">
+            <span className="profiles_dropdown_name block text-15 opacity-70 max-w-nameW overflow-hidden text-ellipsis whitespace-nowrap cursor-pointer">
+              Languages
+            </span>
+          </div>
+        </label>
+
+        <ul className="header_ul">
+          <div className="nav_dropl_col_left">
+            {menuLanguages.map((link, index) => {
+              return (
+                <li
+                  key={index}
+                  className="block relative whitespace-nowrap cursor-default leading-5 text-clairs-navLinks hover:text-white"
+                >
+                  <Link
+                    href={link.path}
+                    className="border-none p-3 break-words whitespace-normal block text-14 "
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </div>
+        </ul>
+      </div>
+
+      <div className="px-6">
+        <input type="checkbox" id="nav_profiles_dropdown_logger" />
+        <label
+          htmlFor="nav_profiles_dropdown_logger"
+          className="nav_profiles_dropdown_logger flex items-center relative pb-0 pl-0 font-normal m-0 border-none cursor-pointer"
+        >
+          <div className="flex items-center shrink-0 cursor-pointer opacity-70">
+            <img src="./welcome/user-24.png" alt="user-icon" />
+          </div>
+          <div className="mx-2 overflow-hidden text-left text-ellipsis font-normal cursor-pointer">
+            <span className="profiles_dropdown_name block text-15 opacity-70 max-w-nameW overflow-hidden text-ellipsis whitespace-nowrap cursor-pointer">
+              Menu
+            </span>
+          </div>
+        </label>
+
+        <ul className="header_ul">
+          <div className="nav_dropl_col_left">
+            {menuLogin.map((link, index) => {
+              return (
+                <li
+                  key={index}
+                  className="block relative whitespace-nowrap cursor-default leading-5 text-clairs-navLinks hover:text-white"
+                >
+                  <Link
+                    href={link.path}
+                    className="border-none p-3 break-words whitespace-normal block text-14 "
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </div>
+        </ul>
+      </div>
+    </div>
   );
 };
