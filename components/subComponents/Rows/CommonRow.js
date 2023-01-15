e85cdd6531f8a86fd0a52f9a858d1a88910e2972
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2023-01-15 20:48:09 +0100

    Utilisation du nouveau Carousel

--- components/subComponents/Rows/CommonRow.js
+++ components/subComponents/Rows/CommonRow.js
@@ -3,7 +3,8 @@
 import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
+import { Carousel } from "@trendyol-js/react-carousel";
+// import { Carousel } from "react-responsive-carousel";
 import { createElement } from "react";
 
 const wrapperNode = (toWrap, wrapper) => {
@@ -15,9 +16,9 @@ export default function CommonRow({ title, pt, titleAlign, props }) {
   wrapperNode();
 
   return (
-    <div className={`u_collect text-white pb-6` + ` ` + pt}>
+    <div className={`u_collect relative text-white pb-6` + ` ` + pt}>
       <div className="u_coll_container ">
-        <div className="title_container  mx-12 mb-2 leading-6">
+        <div className="title_container mx-12 mb-2 leading-6">
           <div className="pe7 flex items-center">
             <div className="logo_container">
               <span>
@@ -45,7 +46,27 @@ export default function CommonRow({ title, pt, titleAlign, props }) {
         </div>
         {/* Astuce à revoir: on enlève mb-[-43px] à une div interne (dont la height ne bougera pas) et cela va impacter la height d'une div plus hautement située dans le code : à NOTER DANS NOTION */}
         <div className="card_carousel_container relative mb-[-43px]">
-          <Carousel showIndicators={false} showArrows={false}>
+          <Carousel showIndicators={false} showArrows={true}>
+            <div className="cont_rev relative inline-block align-top w-full h-full">
+              <div className="reveal relative ">
+                <div className="capsule w-auto h-auto rounded-[3px]">
+                  <a href="">
+                    <picture>
+                      <img src="/film-data/jackryan.jpg" alt="" />
+                    </picture>
+                  </a>
+                </div>
+                <div className="desc">
+                  <div>
+                    <h2>Here is the description</h2>
+                  </div>
+                  <div>
+                    <p>And here the title</p>
+                  </div>
+                  <div>And there the logos</div>
+                </div>
+              </div>
+            </div>
             <div className="cont_rev relative inline-block align-top w-full h-full">
               <div className="reveal">
                 <div className="capsule w-auto h-auto rounded-[3px]">
@@ -66,103 +87,164 @@ export default function CommonRow({ title, pt, titleAlign, props }) {
                 </div>
               </div>
             </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="film-data\anneaux-pouvoir.jpg" alt="" />
-                  </picture>
-                </a>
+            <div className="cont_rev relative inline-block align-top w-full h-full">
+              <div className="reveal">
+                <div className="capsule w-auto h-auto rounded-[3px]">
+                  <a href="">
+                    <picture>
+                      <img src="/film-data/jackryan.jpg" alt="" />
+                    </picture>
+                  </a>
+                </div>
+                <div className="desc">
+                  <div>
+                    <h2>Here is the description</h2>
+                  </div>
+                  <div>
+                    <p>And here the title</p>
+                  </div>
+                  <div>And there the logos</div>
+                </div>
               </div>
             </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\celebrity.jpg" alt="" />
-                  </picture>
-                </a>
+            <div className="cont_rev relative inline-block align-top w-full h-full">
+              <div className="reveal">
+                <div className="capsule w-auto h-auto rounded-[3px]">
+                  <a href="">
+                    <picture>
+                      <img src="/film-data/jackryan.jpg" alt="" />
+                    </picture>
+                  </a>
+                </div>
+                <div className="desc">
+                  <div>
+                    <h2>Here is the description</h2>
+                  </div>
+                  <div>
+                    <p>And here the title</p>
+                  </div>
+                  <div>And there the logos</div>
+                </div>
               </div>
             </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\chemins-liberte.jpg" alt="" />
-                  </picture>
-                </a>
+            <div className="cont_rev relative inline-block align-top w-full h-full">
+              <div className="reveal">
+                <div className="capsule w-auto h-auto rounded-[3px]">
+                  <a href="">
+                    <picture>
+                      <img src="/film-data/jackryan.jpg" alt="" />
+                    </picture>
+                  </a>
+                </div>
+                <div className="desc">
+                  <div>
+                    <h2>Here is the description</h2>
+                  </div>
+                  <div>
+                    <p>And here the title</p>
+                  </div>
+                  <div>And there the logos</div>
+                </div>
               </div>
             </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\dmz.jpg" alt="" />
-                  </picture>
-                </a>
+            <div className="cont_rev relative inline-block align-top w-full h-full">
+              <div className="reveal">
+                <div className="capsule w-auto h-auto rounded-[3px]">
+                  <a href="">
+                    <picture>
+                      <img src="/film-data/jackryan.jpg" alt="" />
+                    </picture>
+                  </a>
+                </div>
+                <div className="desc">
+                  <div>
+                    <h2>Here is the description</h2>
+                  </div>
+                  <div>
+                    <p>And here the title</p>
+                  </div>
+                  <div>And there the logos</div>
+                </div>
               </div>
             </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\faucon-noir.jpg" alt="" />
-                  </picture>
-                </a>
+            <div className="cont_rev relative inline-block align-top w-full h-full">
+              <div className="reveal">
+                <div className="capsule w-auto h-auto rounded-[3px]">
+                  <a href="">
+                    <picture>
+                      <img src="/film-data/jackryan.jpg" alt="" />
+                    </picture>
+                  </a>
+                </div>
+                <div className="desc">
+                  <div>
+                    <h2>Here is the description</h2>
+                  </div>
+                  <div>
+                    <p>And here the title</p>
+                  </div>
+                  <div>And there the logos</div>
+                </div>
               </div>
             </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\hawa.jpg" alt="" />
-                  </picture>
-                </a>
+            <div className="cont_rev relative inline-block align-top w-full h-full">
+              <div className="reveal">
+                <div className="capsule w-auto h-auto rounded-[3px]">
+                  <a href="">
+                    <picture>
+                      <img src="/film-data/jackryan.jpg" alt="" />
+                    </picture>
+                  </a>
+                </div>
+                <div className="desc">
+                  <div>
+                    <h2>Here is the description</h2>
+                  </div>
+                  <div>
+                    <p>And here the title</p>
+                  </div>
+                  <div>And there the logos</div>
+                </div>
               </div>
             </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\high-castle.jpg" alt="" />
-                  </picture>
-                </a>
+            <div className="cont_rev relative inline-block align-top w-full h-full">
+              <div className="reveal">
+                <div className="capsule w-auto h-auto rounded-[3px]">
+                  <a href="">
+                    <picture>
+                      <img src="/film-data/jackryan.jpg" alt="" />
+                    </picture>
+                  </a>
+                </div>
+                <div className="desc">
+                  <div>
+                    <h2>Here is the description</h2>
+                  </div>
+                  <div>
+                    <p>And here the title</p>
+                  </div>
+                  <div>And there the logos</div>
+                </div>
               </div>
             </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\la-route.jpg" alt="" />
-                  </picture>
-                </a>
-              </div>
-            </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\manhunt.jpg" alt="" />
-                  </picture>
-                </a>
-              </div>
-            </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\peacemaker.jpg" alt="" />
-                  </picture>
-                </a>
-              </div>
-            </div>
-            <div className="relative inline-block overflow-hidden align-top w-full h-full">
-              <div className="relative w-auto h-auto overflow-hidden rounded-[3px]">
-                <a href="">
-                  <picture>
-                    <img src="\film-data\vampire-academy.jpg" alt="" />
-                  </picture>
-                </a>
+            <div className="cont_rev relative inline-block align-top w-full h-full">
+              <div className="reveal">
+                <div className="capsule w-auto h-auto rounded-[3px]">
+                  <a href="">
+                    <picture>
+                      <img src="/film-data/jackryan.jpg" alt="" />
+                    </picture>
+                  </a>
+                </div>
+                <div className="desc">
+                  <div>
+                    <h2>Here is the description</h2>
+                  </div>
+                  <div>
+                    <p>And here the title</p>
+                  </div>
+                  <div>And there the logos</div>
+                </div>
               </div>
             </div>
           </Carousel>
